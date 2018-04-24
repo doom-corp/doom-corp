4d63ee849f9f780962ba9805a2054d1d03b10859
aacecandev <aacecan.dev@gmail.com>
2018-04-24 15:56:38 +0200

    profile layout

--- app.js
+++ app.js
@@ -62,7 +62,16 @@ hbs.registerHelper('ifUndefined', (value, options) => {
       return options.fn(this);
   }
 });
-  
+
+Handlebars.registerHelper("equal", function(lvalue, rvalue, options) {
+  if (arguments.length < 3)
+    throw new Error("Handlebars Helper equal needs 2 parameters");
+  if (lvalue != rvalue) {
+    return options.inverse(this);
+  } else {
+    return options.fn(this);
+  }
+});
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
