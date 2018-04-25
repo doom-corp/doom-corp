1ba933e31d4c36a15fb3cbe4b1d1280089f63c1f
Tanis Pan <e.pan.gar@gmail.com>
2018-04-25 21:21:26 +0200

    Mapa arreglado

--- public/javascripts/googleMap.js
+++ public/javascripts/googleMap.js
@@ -1,135 +1,44 @@
-      var markersArray = []
-      var iterator=0
+    var locations = [
+      ["DoomCorp", 40.445354, -3.73492, 6]
+      
+    ];
+function initMap () {
+   
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(40.445354, -3.73492),
+    });
+    
+    var infowindow = new google.maps.InfoWindow();
+    
+    var marker, i;
+    var markers = [];
 
-      function initMap() {
-        const map = new google.maps.Map(document.getElementById('map'), {
-          center: {
-            lat: 40.445354, 
-            lng: -3.73492
-          }, 
-          zoom: 15
-        });
-        
-        const doomCorpMarker = new google.maps.Marker({
-          position: {
-            lat: 40.446221,
-            lng: -3.733238
-          },
-          map: map,
-          title: "DOOMCORP :D"
-        });
+    const input = /** @type {!HTMLInputElement} */(document.getElementById('pac-input'));
+    
+    const autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
 
-        const input = /** @type {!HTMLInputElement} */(document.getElementById('pac-input'));
-        
-        const types = document.getElementById('type-selector');
-        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+    
 
-        const autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map);
-
-        const infowindow = new google.maps.InfoWindow();
-        
-        const marker = new google.maps.Marker({
-          map: map,
-          anchorPoint: new google.maps.Point(0, 0),
-
-        });
-
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          const place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
-
-          
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(15);  
-          }
-
-          marker.setIcon(/** @type {google.maps.Icon} */({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35),
-            
-          }));
-          markersArray.forEach((mar) => {
-            console.log("en el foreach")
-            console.log(mar)
-            marker.setPosition(mar);
+    autocomplete.addListener('place_changed', function() {
+            infowindow.close();
+            marker.setVisible(false);
+            const place = autocomplete.getPlace();
+            if (!place.geometry) {
+              // User entered the name of a Place that was not suggested and
+              // pressed the Enter key, or the Place Details request failed.
+              window.alert("No details available for input: '" + place.name + "'");
+              return;
+            }
           })
 
 
-          // marker.setPosition(place.geometry.location);
-          console.log(place.geometry.location)
-          marker.setVisible(true);
-
-          let address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-          infowindow.open(map, marker);
-
-        });
-
-        // Sets a listener on a radio button to change the filter type on Places
-        // Autocomplete.
-        function setupClickListener(id, types) {
-          const radioButton = document.getElementById(id);
-          radioButton.addEventListener('click', function() {
-            autocomplete.setTypes(types);
-          });
-        }
-
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function (position) {
-            const user_location = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-      
-            // Center map with user location
-            map.setCenter(user_location);
-      
-            // Add a marker for your user location
-            const doomCorpMarker = new google.maps.Marker({
-              position: {
-                lat: user_location.lat,
-                lng: user_location.lng
-              },
-              map: map,
-              title: "Usted está aquí, nosotros no."
-            });
-      
-          }, function () {
-            console.log('You are here, we\'re not.');
-          });
-        } else {
-          console.log('Browser does not support geolocation.');
-        }
-
-        
+    
          var geocoder = new google.maps.Geocoder();
        
-        //var direction = "Madrid";
+        
         document.getElementById("pac-input").addEventListener("change", function(){
           var direction = document.getElementById("pac-input").value;
           console.log("direction: " + direction);
@@ -142,26 +51,23 @@
               console.log(latitude + "," + longitude)
             } 
             function addMarkers(lat, log){
-              markersArray.push({lat: lat, lng: log});
-              console.log(markersArray)
+              locations.push(["NOMBRE", lat, log, 8]);
+              console.log(locations)
             }
             addMarkers(latitude, longitude);
-          }); 
-
-          markersArray.forEach(arr => {
-            // var marker = new.google.maps.Marker({
-            //   position: arr,
-            //   map: map
-            // })
+            initMap()
           })
-          
+         
         })
-        
-        setupClickListener('changetype-all', []);
-        setupClickListener('changetype-address', ['address']);
-        setupClickListener('changetype-geocode', ['geocode']);
-        
-      }
 
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+    
+      markers.push(marker);
+      console.log(marker)
+    }
 
-
+  }
