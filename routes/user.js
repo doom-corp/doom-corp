677e81f2b76fb6de702b37cd4f79f5296f1a725a
aacecandev <aacecan.dev@gmail.com>
2018-04-25 16:55:30 +0200

    generating primitive pdf

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const User = require("../models/User");
 const sendPassChangedMail = require("../mail/sendPassChangedMail");
 const ensureLoggedIn = require("../middlewares/ensureLoggedIn");
+const pdfDocument = require('pdfkit');
 
 
 // Bcrypt to encrypt passwords
@@ -12,6 +13,9 @@ const bcryptSalt = 10;
 
 /* GET user profile */
 router.get("/", ensureLoggedIn("/"), (req, res, next) => {
+
+  // METER IFS PARA LOS DISTINTOS TIPOS DE USUARIOS
+
   res.render("user/profile", { user: req.user });
 });
 
@@ -58,7 +62,31 @@ router.post("/changepass", ensureLoggedIn("/"),(req, res, next) => {
     .catch(err => console.log("User not found:" + err));
 });
 
-router.get("/pdf")
+router.get("/pdf", (req, res, next) => {
+  let doc = new pdfDocument();
+  doc.pipe(res);
+  doc
+    .moveTo(300, 75)
+    .lineTo(373, 301)
+    .lineTo(181, 161)
+    .lineTo(419, 161)
+    .lineTo(227, 301)
+    .fill("red", "even-odd");
+
+  var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in...";
+
+  doc.y = 320;
+  doc.fillColor("black");
+  doc.text(loremIpsum, {
+    paragraphGap: 10,
+    indent: 20,
+    align: "justify",
+    columns: 2
+  });
+
+  //res.download("out.pdf");
+  doc.end();
+})
 
 
 module.exports = router;
