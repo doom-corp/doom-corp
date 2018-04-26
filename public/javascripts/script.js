ea080981fa923f468cfcd5bcab77f44c525924d5
aacecandev <aacecan.dev@gmail.com>
2018-04-26 16:20:33 +0200

    fixed jquery profile show/hide

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  $("#map-wrapper").click(function() {
+  $("#show-map-btn").click(function() {
     $("#map-wrapper").show();
     $("#pass-changer-wrapper").hide();
-    $(".pdf").hide();
+    $("#pdf").hide();
   });
 
   $("#payslip").click(function() {
     $("#map-wrapper").hide();
     $("#pass-changer-wrapper").show();
-    $(".pdf").hide();
+    $("#pdf").hide();
   });
 
   $("#change-pass").click(function() {
     $("#map-wrapper").hide();
     $("#pass-changer-wrapper").hide();
-    $(".pdf").show();
+    $("#pdf").show();
   });
 }, false);
